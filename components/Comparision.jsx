d20dffff80e7f55c1f6f98f97b215ab0e92e4062
DedSec2050 <marsalsoren2050@gmail.com>
2024-06-26 17:53:33 +0530

    completed comparision, guidelines section

--- components/Comparision.jsx
+++ components/Comparision.jsx
@@ -7,15 +7,15 @@ const Page7 = () => {
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-center text-clamptitle">
           <strong>
-            <span className="bg-gradient-to-r from-accent via-grn to-accent bg-clip-text text-transparent">
+            <span className="bg-gradient-to-r from-accent via-grn to-accent bg-clip-text text-transparent xxl:text-[2.5vw]">
               CISSP Certification Bootcamp VS CISSP
             </span>{" "}
-            <span className="bg-gradient-to-r from-accent to-grn bg-clip-text text-transparent">
+            <span className="bg-gradient-to-r from-accent to-grn bg-clip-text text-transparent xxl:text-[2.5vw]">
               Success Toolkit
             </span>
           </strong>
         </h1>
-        <div className="text-clamptextsmm mt-10 w-[84vw] font-medium leading-9">
+        <div className="mt-10 w-[84vw] text-clamptextsmm font-medium leading-9 xxl:text-[1.5vw] xxl:leading-normal">
           CISSP Exam not only test your knowledge but also test your ability to
           take security decisions based on cognitive thinking as a leader. To
           Establish yourself as a Subject Matter Expert (SME) and a thought
@@ -24,12 +24,12 @@ const Page7 = () => {
           Certification bootcamp providing CISSP Training Online is barely
           effective. These CISSP Courses are very high level.
         </div>
-        <div className="text-clamptextsmm mt-[40px] w-[84vw] font-medium leading-9">
+        <div className="mt-[40px] w-[84vw] text-clamptextsmm font-medium leading-9 xxl:text-[1.5vw] xxl:leading-normal">
           CISSP Success Toolkit has been curated with the same intent in mind
           and has been programmed for busy working professionals to successfully
           make this transformation and pass the exam with 10x ease.
         </div>
-        <div className="text-clamptextsmm mt-[40px] w-[84vw] font-medium leading-9">
+        <div className="mb-[3rem] mt-[40px] w-[84vw] text-clamptextsmm font-medium leading-9 xxl:text-[1.5vw] xxl:leading-normal">
           Let me show you why I call this program as a complete Eco-system.
         </div>
         <div className="relative mt-[45px] flex h-[50vh] w-[90%] bg-transparent md:h-[90vh]">
--- components/Guidelines.jsx
+++ components/Guidelines.jsx
@@ -4,21 +4,21 @@ const Guidelines = () => {
   return (
     <section className="relative flex w-full flex-col items-center text-center">
       <div className="flex">
-        <h1 className="bg-gradient-to-r from-grn via-accent to-grn bg-clip-text text-clamptitle font-bold text-transparent">
+        <h1 className="bg-gradient-to-r from-grn via-accent to-grn bg-clip-text text-clamptitle font-bold text-transparent xxl:text-[2.5vw]">
           Others teach you CISSP
         </h1>
       </div>
       <div className="flex translate-y-[-15px]">
-        <h1 className="bg-gradient-to-r from-grn via-accent to-grn bg-clip-text text-clamptitle font-bold text-transparent">
+        <h1 className="bg-gradient-to-r from-grn via-accent to-grn bg-clip-text text-clamptitle font-bold text-transparent xxl:text-[2.5vw]">
           We coach you how to pass CISSP
         </h1>
       </div>
       <div className="flex">
-        <h1 className="text-clamptextmd font-semibold text-accent">
+        <h1 className="text-clamptextmd font-semibold text-accent xxl:text-[1.5vw]">
           Updated as per new guidelines from April 2024
         </h1>
       </div>
-      <div className="flex w-[80vw] py-2 text-clamptextsmm font-medium leading-10">
+      <div className="flex w-[80vw] py-2 text-clamptextsmm font-medium leading-10 xxl:w-[65vw] xxl:text-[1.3vw] xxl:leading-normal">
         (ISC)2 CISSP Exam is tricky and only studying the course content is not
         enough to pass the exam. Believe me, many Aspirants do in-depth CISSP
         Preparation but they still fail the CISSP Certification Exam. The reason
@@ -26,14 +26,14 @@ const Guidelines = () => {
         Unfortunately, most of the training focus on the content but not on the
         Exam taking skills.{" "}
       </div>
-      <div className="flex w-[80vw] py-4 text-clamptextsmm font-medium leading-10">
+      <div className="flex w-[80vw] py-4 text-clamptextsmm font-medium leading-10 xxl:w-[65vw] xxl:text-[1.3vw] xxl:leading-normal">
         CISSP Success Toolkit is the one and only program which trains you for
         the challenging CISSP exam through our 60+hours of Live Exam practice.
       </div>
-      <div className="flex py-4 text-center text-clamptextsmm font-medium leading-10">
+      <div className="flex justify-center py-4 text-center text-clamptextsmm font-medium leading-10 xxl:w-[65vw] xxl:text-[1.3vw] xxl:leading-normal">
         This is just one feature, let me tell you more.
       </div>
-      <div className="mb-7 mt-8 h-[52vh] w-[62vw] overflow-hidden rounded-[25px] border-2 border-accent bg-gray-600"></div>
+      <div className="mb-7 mt-8 h-[52vh] w-[62vw] overflow-hidden rounded-[25px] border-2 border-accent bg-gray-600 xxl:max-h-[750px] xxl:w-[55vw] xxl:max-w-[1400px]"></div>
     </section>
   );
 };
