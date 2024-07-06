da65bdcb631b778540d58112680a1bb61a8a9f2a
DedSec2050 <marsalsoren2050@gmail.com>
2024-07-06 22:05:00 +0530

    removed legacy import

--- components/HeroSlider.jsx
+++ components/HeroSlider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/legacy/image";
+import Image from "next/image";
 import "../components/css/tooltip.css";
 import img from "../public/assets/MarqueeAssets/tags-one.webp";
 const HeroSlider = () => {
