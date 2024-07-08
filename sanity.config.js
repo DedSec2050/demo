0645063991825fb6dffe9601e91c707f1a831422
DedSec2050 <marsalsoren2050@gmail.com>
2024-07-09 00:21:50 +0530

    sanity

--- sanity.config.js
+++ sanity.config.js
@@ -9,12 +9,12 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
+import { apiVersion, dataset, projectId } from "./sanity/env.local";
 import { schema } from "./sanity/schema";
 
 export default defineConfig({
   basePath: "/studio",
-  projectId: "qhux44z1",
+  projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
