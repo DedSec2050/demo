435c065b05ac032b8c7dc60355b68bd0a8ce3c3b
DedSec2050 <marsalsoren2050@gmail.com>
2024-07-14 14:22:32 +0530

    adjusted some bug fixes

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -25,7 +25,7 @@ const Blog = async () => {
     <div>
       <Banner />
       <Header />
-      <section className="flex flex-col items-center justify-center p-24 md:p-8">
+      <section className="flex flex-col items-center justify-center p-4 md:p-8 lg:p-20">
         <h1 className="font-700 mb-[30px] text-[40px] font-bold text-accent">
           CyberSecurity Certification Blogs
         </h1>
