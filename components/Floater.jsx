4e255a4ecaac3830b9a9104c4e591b3ea2f478e4
DedSec2050 <marsalsoren2050@gmail.com>
2024-07-14 08:54:48 +0530

    Floater Modified

--- components/Floater.jsx
+++ components/Floater.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import float from "../public/assets/floating.webp";
-import Image from "next/image";
+import { TiSocialInstagramCircular } from "react-icons/ti";
+import { SocialIcon } from "react-social-icons";
+
 const Floater = () => {
   return (
-    <div className="floating fixed bottom-[12vh] right-[2vw] z-10 h-[3rem] w-[3rem] rounded-full border-2 border-white p-2 hover:cursor-pointer md:h-[3.5rem] md:w-[3.5rem] xxl:h-[5vw] xxl:w-[5vw]">
-      <Image
-        layout="responsive"
-        height={100}
-        width={100}
-        src={float}
-        alt="hero-trainer"
-      />
+    <div className="floating delay-750 fixed bottom-[12vh] right-[2vw] z-10 flex h-[4rem] w-[4rem] flex-col items-center justify-center rounded-full border-2 border-white transition-all hover:h-[250px] hover:cursor-pointer active:h-[200px] xxl:h-[5vw] xxl:w-[5vw] [&:hover>.floater]:translate-y-[-20px] [&:hover>.floater]:opacity-0 [&:hover>div]:opacity-100">
+      <div className="floater absolute transition-opacity">
+        <SocialIcon url="https://shareit.com" />
+      </div>
+      <div className="relative flex flex-col gap-3 opacity-0 transition-opacity [&>a:hover]:scale-105">
+        <SocialIcon url="https://whatsapp.com" />
+        <SocialIcon url="https://instagram.com" />
+        <SocialIcon url="https://facebook.com" />
+        <SocialIcon url="https://youtube.com" />
+      </div>
     </div>
   );
 };
