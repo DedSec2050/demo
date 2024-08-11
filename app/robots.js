5679c7e8c65283a9e48c05591837b2841a9a3936
DedSec2050 <marsalsoren2050@gmail.com>
2024-08-11 17:34:29 +0530

    removed robots and sitemap

--- app/robots.js
+++ app/robots.js
@@ -1,12 +1,12 @@
-export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/studio", "/private"],
-      },
-    ],
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
-  };
-}
+// export default function robots() {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//         disallow: ["/studio", "/private"],
+//       },
+//     ],
+//     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+//   };
+// }
