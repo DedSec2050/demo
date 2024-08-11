712ba43782aafbdd7f6c8d7370d913f310f90b2e
DedSec2050 <marsalsoren2050@gmail.com>
2024-08-11 09:41:53 +0530

    added rule validation as required for all fields in sanity

--- sanity/schemas/blog.js
+++ sanity/schemas/blog.js
@@ -18,21 +18,25 @@ export default {
       options: {
         source: "title",
       },
+      validation: (rule) => [rule.required()],
     },
     {
       name: "titleImage",
       type: "image",
       title: "Title Image",
+      validation: (rule) => [rule.required()],
     },
     {
       name: "smallDescription",
       type: "text",
       title: "Small Description",
+      validation: (rule) => [rule.required()],
     },
     {
       name: "content",
       type: "array",
       title: "Content",
+      validation: (rule) => [rule.required()],
       of: [
         {
           type: "block",
