e12394c78b7a05bbd7f36cb0184827fb17be1f62
DedSec2050 <marsalsoren2050@gmail.com>
2024-08-14 19:54:12 +0530

    added phone number field

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,13 +1,76 @@
+"use client";
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./contact.css";
 import BackWrapper from "@/components/BackWrapper";
 import Floater from "@/components/Floater";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+import { parsePhoneNumberFromString } from "libphonenumber-js";
+import {
+  isPossiblePhoneNumber,
+  isValidPhoneNumber,
+  validatePhoneNumberLength,
+} from "libphonenumber-js";
+import { Phone } from "lucide-react";
 
 const ContactUs = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  // const [fname, setFname] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [query, setQuery] = useState("");
+  const email = useRef(null);
+  const query = useRef(null);
+  const fname = useRef(null);
+  const phno = useRef(null);
+
+  const [valid, setValid] = useState(false);
+  const handleChange = (value) => {
+    setPhoneNumber(value);
+    console.log(value);
+    setValid(validatePhoneNumber(value));
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneNumberObject = parsePhoneNumberFromString("+" + phoneNumber);
+    console.log(phoneNumberObject);
+    if (phoneNumberObject && phoneNumberObject.isValid) {
+      return phoneNumberObject.isValid();
+    }
+
+    return false;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(phno);
+    try {
+      if (!valid) {
+        alert("Enter valid Ph no");
+        return;
+      }
+      console.log(email.current.value);
+      console.log(fname.current.value);
+      console.log(parsePhoneNumberFromString("+" + phoneNumber).nationalNumber);
+
+      console.log(query.current.value);
+      alert("Response Submitted");
+    } catch (e) {
+      console.log(e);
+      alert("Enter all the fields");
+    }
+
+    // if (validatePhoneNumber(document.getElementById("contact-phone").value))
+    // console.log(value);
+  };
+
+  // Collecting values
+  // const handleFname()
+
   return (
     <section>
       <Banner />
@@ -24,39 +87,87 @@ const ContactUs = () => {
             className="items-center justify-center rounded-lg text-black sm:border-2 sm:bg-elevated sm:px-4 md:ml-[20%] md:px-8"
           >
             <div className="input-box my-4 flex flex-col gap-y-2">
-              <label htmlFor="" className="font-semibold text-white">
+              <label
+                for="contact-name"
+                htmlFor=""
+                className="font-semibold text-white"
+              >
                 Full Name
               </label>
               <input
+                id="contact-name"
                 type="text"
                 className="field w-[280px] rounded-md p-1 active:bg-accent md:max-w-[350px]"
                 placeholder="Enter your name"
+                ref={fname}
                 required
               />
             </div>
             <div className="input-box my-4 flex flex-col gap-y-2">
-              <label htmlFor="" className="font-semibold text-white">
+              <label
+                for="contact-email"
+                htmlFor=""
+                className="font-semibold text-white"
+              >
                 Email Address
               </label>
               <input
+                id="contact-email"
                 type="email"
                 className="w-[280px] rounded-md p-1 active:bg-accent md:max-w-[350px]"
                 placeholder="Enter your email"
+                ref={email}
                 required
               />
             </div>
             <div className="input-box my-4 flex flex-col gap-y-2">
-              <label htmlFor="" className="font-semibold text-white">
+              <label
+                for="contact-phone"
+                htmlFor=""
+                className="font-semibold text-white"
+              >
+                Phone number
+              </label>
+              <PhoneInput
+                id="contact-phone"
+                country="in"
+                type="text"
+                className="w-[280px] rounded-md p-1 active:bg-accent md:max-w-[350px]"
+                inputProps={{
+                  required: true,
+                }}
+                value={phoneNumber}
+                onChange={handleChange}
+                ref={phno}
+                placeholder="Enter your Phone number"
+              />
+              {!valid && (
+                <p className="text-red-300">Enter a valid Phone number</p>
+              )}
+            </div>
+            <div className="input-box my-4 flex flex-col gap-y-2">
+              <label
+                for="contact-queries"
+                htmlFor=""
+                className="font-semibold text-white"
+              >
                 Your Queries
               </label>
               <textarea
+                id="contact-queries"
                 cols="40"
                 className="field max-w-[400px] rounded-md p-1 pb-10 text-black active:bg-accent"
                 placeholder="Enter your Queries"
+                ref={query}
                 required
               />
             </div>
-            <Button variant="outline" className="my-6" type="submit">
+            <Button
+              variant="outline"
+              className="my-6"
+              type="submit"
+              onClick={handleSubmit}
+            >
               Submit
             </Button>
           </form>
