32be3080d323f235b46a1bfefb0f75282625ca32
DedSec2050 <marsalsoren2050@gmail.com>
2024-08-16 16:55:00 +0530

    added Scripts

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,12 @@ import "./globals.css";
 // Components
 
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata = {
   metadataBase: new URL("https://www.cybernous.com/"),
   title: {
-    default: "Cybernous CISSP Online Training",
+    default: "CISSP Course in Bangalore",
     template: `%s | Cybernous`,
   },
   description:
@@ -66,70 +67,100 @@ export const metadata = {
 // JSONLD
 const jsonLd = {
   "@context": "https://schema.org",
-  "@type": "WebSite",
-  name: "Cybernous CISSP Online Training",
+  "@type": "LocalBusiness",
+  name: "cybernous",
+  image:
+    "https://s3.us-east1.amazonaws.com/contents.newzenler.com/37503/library/65c38f65e0118_1707315045_cybernous-log-with-new-brand-color.png",
+  "@id": "",
   url: "https://www.cybernous.com/",
-  potentialAction: {
-    "@type": "SearchAction",
-    target: "https://www.cybernous.com/search?q={search_term_string}",
-    "query-input": "required name=search_term_string",
+  telephone: "+91 8595338705",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress:
+      "No. A/167, Bagalur Road, Vinayak Nagar, Near Yelahanka,Palanahalli, Bengaluru, Bengaluru Urban, Karnataka, 560064",
+    addressLocality: "India",
+    postalCode: "560064",
+    addressCountry: "India",
   },
-  sameAs: [
-    "https://www.facebook.com/YourFacebookPage",
-    "https://www.linkedin.com/company/YourLinkedInPage",
-    "https://twitter.com/Marsal_Soren06",
-  ],
-  publisher: {
-    "@type": "Organization",
-    name: "Cybernous",
-    url: "https://www.cybernous.com/",
-    logo: {
-      "@type": "ImageObject",
-      url: "https://demo-gamma-ebon.vercel.app/assets/og-image/image.png",
-      width: 1200,
-      height: 630,
-    },
-  },
-  image: {
-    "@type": "ImageObject",
-    url: "https://demo-gamma-ebon.vercel.app/assets/og-image/image.png",
-    width: 1200,
-    height: 630,
+  geo: {
+    "@type": "GeoCoordinates",
+    latitude: 13.107568,
+    longitude: 77.571198,
   },
 };
 const jsonLdBlogs = {
   "@context": "https://schema.org",
-  "@type": "BlogPosting",
-  name: "Blogs | Cybernous CISSP Online Training",
-  url: "https://www.cybernous.com/blog",
-  publisher: {
-    "@type": "Organization",
-    name: "Cybernous",
-    url: "https://www.cybernous.com/",
-    logo: {
-      "@type": "ImageObject",
-      url: "https://demo-gamma-ebon.vercel.app/assets/og-image/image.png",
-      width: 1200,
-      height: 630,
-    },
+  "@type": "Organization",
+  name: "cybernous",
+  alternateName: "cybernous",
+  url: "https://www.cybernous.com/",
+  logo: "https://s3.us-east1.amazonaws.com/contents.newzenler.com/37503/library/65c38f65e0118_1707315045_cybernous-log-with-new-brand-color.png",
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "8595338705",
+    contactType: "sales",
+    areaServed: "India",
+    availableLanguage: "en",
   },
-  image: {
-    "@type": "ImageObject",
-    url: "https://demo-gamma-ebon.vercel.app/assets/og-image/image.png",
-    width: 1200,
-    height: 630,
-  },
-  author: {
-    "@type": "Person",
-    name: "Manoj Sharma",
-  },
-  headline: "Title of the Blog Post",
-  description: "A brief description of the blog post.",
+  sameAs: [
+    "https://www.facebook.com/cybernous",
+    "https://www.youtube.com/channel/UC43m9FCBm8cpZFnO9NCU2eg",
+    "https://www.instagram.com/cybernous_india",
+    "https://www.linkedin.com/in/mksharma007/",
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* LinkedIn Tag Manager */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "5937948";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `,
+          }}
+        />
+        <meta
+          name="google-site-verification"
+          content="3V77gokHFiFVtwpFrn6FH7tDTq8Q9KwUlNJG05_an0"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16584736238"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16584736238');
+            `,
+          }}
+        />
+        <script src="https://cdnin.pagesense.io/js/cybernous8/9feb95c098fc4684b0369d37356bb969.js"></script>
+      </Head>
       <body className="bg-primary">
         <Script
           id="website-schema"
@@ -139,7 +170,7 @@ export default function RootLayout({ children }) {
           }}
         />
         <Script
-          id="blogs-schema"
+          id="organization-schema"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(jsonLdBlogs),
@@ -147,8 +178,21 @@ export default function RootLayout({ children }) {
         />
 
         {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=GJE148Z2662"
+        ></Script>
 
         <Script src="https://checkout.razorpay.com/v1/checkout.js"></Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JE148Z2662"
+        ></Script>
+        <Script type="text/javascript">
+          _linkedin_partner_id = "5937948"; window._linkedin_data_partner_ids =
+          window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        </Script>
       </body>
     </html>
   );
