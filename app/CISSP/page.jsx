6ebcae87611150dbef5ea4c8abea5d7baf4cb422
DedSec2050 <marsalsoren2050@gmail.com>
2024-09-19 14:24:20 +0530

    Made an immediate copy at /CISSP for immediate production

--- app/CISSP/page.jsx
+++ app/CISSP/page.jsx
@@ -1,21 +1,20 @@
 import Hero from "@/components/Pages/Hero";
 // import Popup from "@/components/Popup";
 import "animate.css";
-import BootCampSlider from "@/components/BootCampSlider";
-import Schedule from "@/components/Schedule";
-import Roadmap from "@/components/Roadmap";
-import Comparision from "@/components/Comparision";
-import Testimonials from "@/components/Testimonials";
-import AccordionWrap from "@/components/AccordionWrap";
+import BootCampSlider from "../components/BootCampSlider";
+import Schedule from "../components/Schedule";
+import Roadmap from "../components/Roadmap";
+import Comparision from "../components/Comparision";
+import Testimonials from "../components/Testimonials";
+import AccordionWrap from "../components/AccordionWrap";
 import Guidelines from "@/components/Guidelines";
 import Connect from "@/components/Connect";
-import Resources from "@/components/Resources";
+import Resources from "../components/Resources";
 import Positions from "@/components/Positions";
 import QualityAssr from "@/components/QualityAssr";
 import Benefits from "@/components/Benefits";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Popup from "@/components/Popup";
 import Banner from "@/components/Banner";
 import BackWrapper from "@/components/BackWrapper";
 import Floater from "@/components/Floater";
@@ -25,7 +24,7 @@ import SuccessStories from "@/components/Pages/SuccessStories";
 export default function Home() {
   return (
     <div className="">
-      <BackWrapper />
+      {/* <BackWrapper /> */}
       {/* <Popup /> */}
       {/* <div className="">text</div> */}
       <Banner />
@@ -50,7 +49,7 @@ export default function Home() {
         <Testimonials />
         <QualityAssr />
         <Benefits />
-        <Floater />
+        {/* <Floater /> */}
         <AccordionWrap />
       </main>
       <Footer />
